6db583e385af87294c38b4c84d7174be8d79a89f
abarabonova <55283014+abarabonova@users.noreply.github.com>
2020-04-27 18:30:03 +0300

    основое 2.0

---
+++ anime.js
@@ -0,0 +1,13 @@
+// function openbox(){
+//
+//   display = document.getElementByClass('icon').style.display;
+//   if(display =='inline-block'){
+//      document.getElementByClass('icon').style.display='none';
+//   } else{
+//      document.getElementByClass('icon').style.display='inline-block';
+//   }
+
+function openMenu() {
+  document.getElementById('sidebar').classList.toggle('active');
+
+}
